6a8af290fbb8a377828e9c08be253d38f3ff7822
mathsudre <mathsudre@gmail.com>
2022-12-15 12:19:57 -0300

    add more portfolio infos

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,40 +1,54 @@
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaGit, FaPython } from "react-icons/fa";
 import {
-  FaDocker,
-  FaHtml5,
-  FaCss3Alt,
-  FaJs,
-  FaNode,
-  FaReact,
+	FaCss3Alt,
+	FaDocker,
+	FaGit,
+	FaHtml5,
+	FaJs,
+	FaNode,
+	FaPython,
+	FaReact,
 } from "react-icons/fa";
+import {
+	SiDjango,
+	SiMongodb,
+	SiNestjs,
+	SiPostgresql,
+	SiStyledcomponents,
+	SiTailwindcss,
+	SiTypescript,
+} from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
 export const stackData = [
-  {
-    title: "HTML",
-    img: FaHtml5,
-  },
-  {
-    title: "CSS",
-    img: FaCss3Alt,
-  },
-  {
-    title: "JS",
-    img: FaJs,
-  },
-  { title: "TypeScript", img: SiTypescript },
-  {
-    title: "Node JS",
-    img: FaNode,
-  },
-  {
-    title: "React",
-    img: FaReact,
-  },
-  { title: "Next.js", img: TbBrandNextjs },
-  { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: SiPostgresql },
-  { title: "Git", img: FaGit },
-  { title: "Python", img: FaPython },
+	{
+		title: "HTML",
+		img: FaHtml5,
+	},
+	{
+		title: "CSS",
+		img: FaCss3Alt,
+	},
+	{
+		title: "JS",
+		img: FaJs,
+	},
+	{ title: "TypeScript", img: SiTypescript },
+	{
+		title: "Node JS",
+		img: FaNode,
+	},
+	{
+		title: "React",
+		img: FaReact,
+	},
+	{ title: "Next.js", img: TbBrandNextjs },
+	{ title: "Docker", img: FaDocker },
+	{ title: "PostgreSQL", img: SiPostgresql },
+	{ title: "Git", img: FaGit },
+	{ title: "Python", img: FaPython },
+	{ title: "Django", img: SiDjango },
+	{ title: "Nest.js", img: SiNestjs },
+	{ title: "Mongodb", img: SiMongodb },
+	{ title: "Tailwindcss", img: SiTailwindcss },
+	{ title: "Styledcomponents", img: SiStyledcomponents },
 ];
